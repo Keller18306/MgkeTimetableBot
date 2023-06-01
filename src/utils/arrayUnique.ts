79c28c17549e312b5a31a77418abe53efd3fe09c
Keller <kompeter17@gmail.com>
2023-06-01 13:15:42 +0300

    update arrayUnique

--- src/utils/arrayUnique.ts
+++ src/utils/arrayUnique.ts
@@ -1,11 +1,3 @@
 export function arrayUnique<T>(array: T[]): T[] {
-    const newArray: T[] = []
-
-    for (const value of array) {
-        if (newArray.includes(value)) continue;
-
-        newArray.push(value)
-    }
-
-    return newArray;
+    return Array.from(new Set(array));
 }
