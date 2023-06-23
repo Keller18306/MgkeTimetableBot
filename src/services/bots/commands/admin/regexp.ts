dd19fcad428c10e4a554d45fa6a3ce6c718fc254
Keller <kompeter17@gmail.com>
2023-06-23 20:55:27 +0300

    commands regexp list

---
+++ src/services/bots/commands/admin/regexp.ts
@@ -0,0 +1,29 @@
+import { TelegramBotCommand } from "puregram/generated";
+import { DefaultCommand, HandlerParams } from "../../abstract";
+import { CommandController } from "../../command";
+
+export default class extends DefaultCommand {
+    public id = 'admin_regexp'
+
+    public regexp = /^(!|\/)regexp$/i
+    public payload = null;
+    public adminOnly: boolean = true;
+    public tgCommand: TelegramBotCommand | null = {
+        command: 'regexp',
+        description: 'Отобразить все команды и регулярки к ним'
+    };
+
+    handler({ context }: HandlerParams) {
+        const commands = CommandController.instance.commands;
+
+        const message: string[] = [];
+        for (const id in commands) {
+            const { command } = commands[id];
+            const regexp: string = String(command.regexp);
+
+            message.push(`${id}: ${regexp}`)
+        }
+
+        return context.send(message.join('\n'));
+    }
+}
