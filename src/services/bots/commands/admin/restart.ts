c09a703432ae5010ccc155910dd23813b73b88ff
Keller <kompeter17@gmail.com>
2023-09-07 16:02:15 +0300

    restart description

--- src/services/bots/commands/admin/restart.ts
+++ src/services/bots/commands/admin/restart.ts
@@ -1,10 +1,16 @@
 import { execSync } from "child_process";
 import { AbstractCommand, HandlerParams } from "../../abstract";
+import { TelegramBotCommand } from "puregram/generated";
 
 export default class extends AbstractCommand {
     public regexp = /^(!|\/)restart$/i
     public payload = null;
     public adminOnly: boolean = true;
+
+    public tgCommand: TelegramBotCommand = {
+        command: 'restart',
+        description: 'Перезапуск бота (только при использовании PM2)'
+    };
 
     async handler({ context, service }: HandlerParams) {
         await context.send('Restarting...');
