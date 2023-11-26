bb15fe437a449c9865a64f7e05db81d4506856ea
Keller <kompeter17@gmail.com>
2023-11-27 00:44:14 +0300

    storage conflict fix

--- src/db/storage.ts
+++ src/db/storage.ts
@@ -5,5 +5,6 @@ export function getValueFromStorageByKey(storage: string, key: string): any {
 }
 
 export function addValueIntoStorageByKey(storage: string, key: string, value: string, time: number): void {
-    db.prepare('INSERT INTO storage (`storage`, `key`, `value`, `time`) VALUES (?, ?, ?, ?)').run(storage, key, value, time);
+    db.prepare('INSERT INTO storage (`storage`, `key`, `value`, `time`) VALUES (:storage, :key, :value, :time) ON CONFLICT(`storage`, `key`) DO UPDATE SET value = :value, time = :time')
+        .run({ storage, key, value, time });
 }
