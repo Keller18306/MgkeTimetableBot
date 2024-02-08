2df6c6cc8b59512534b7b4b4a9488131f15aef3e
Keller <kompeter17@gmail.com>
2024-02-08 22:15:04 +0300

    api info

---
+++ src/services/api/methods/info.ts
@@ -0,0 +1,32 @@
+import StatusCode from "status-code-enum";
+import { raspCache } from "../../../updater";
+import { StringDate } from "../../../utils";
+import VKAppDefaultMethod, { HandlerParams } from "./_default";
+
+export default class VkAppAuthMethod extends VKAppDefaultMethod {
+    public httpMethod: "GET" | "POST" = 'GET';
+    public method: string = 'info';
+
+    handler({ request, response }: HandlerParams) {
+        return {
+            groups: {
+                update: raspCache.groups.update,
+                changed: raspCache.groups.changed,
+                lastWeekIndex: raspCache.groups.lastWeekIndex,
+                hash: raspCache.groups.hash
+            },
+            teachers: {
+                update: raspCache.teachers.update,
+                changed: raspCache.teachers.changed,
+                lastWeekIndex: raspCache.teachers.lastWeekIndex,
+                hash: raspCache.teachers.hash
+            },
+            team: {
+                update: raspCache.team.update,
+                changed: raspCache.team.changed,
+                hash: raspCache.team.hash
+            },
+            lastSuccess: raspCache.successUpdate
+        }
+    }
+}
