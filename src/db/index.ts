5feab981515a5f86b23d572a450012979eec9034
Keller <kompeter17@gmail.com>
2024-05-19 20:57:53 +0300

    disable sql logging for prod

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,8 @@
-import { Sequelize } from 'sequelize';
+import { Options, Sequelize } from 'sequelize';
 import { config } from '../../config';
-export const sequelize = new Sequelize(config.db);
+
+export const sequelize = new Sequelize(Object.assign<Options, Options>(config.db, {
+    logging: config.dev // Логируем все SQL запросы в режиме разработки
+}));
 
 export * from './clean';
