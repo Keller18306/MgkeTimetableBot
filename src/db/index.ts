f8cc2768d093f13f7905cf8c23fa40c6c66c92ec
Keller <kompeter17@gmail.com>
2024-05-20 01:02:35 +0300

    sequelize logging fix

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,7 +2,7 @@ import { Options, Sequelize } from 'sequelize';
 import { config } from '../../config';
 
 export const sequelize = new Sequelize(Object.assign<Options, Options>(config.db, {
-    logging: config.dev // Логируем все SQL запросы в режиме разработки
+    logging: config.dev ? console.log : false // Логируем все SQL запросы в режиме разработки
 }));
 
 export * from './clean';
