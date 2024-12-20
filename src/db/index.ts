c5fcde8fa3ed1c75583258c6b4820c4cf88aca96
Keller <kompeter17@gmail.com>
2024-12-20 18:05:10 +0300

    sequelize override logger with config

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,8 @@
 import { Options, Sequelize } from 'sequelize';
 import { config } from '../../config';
 
-export const sequelize = new Sequelize(Object.assign<Options, Options>(config.db, {
+export const sequelize = new Sequelize(Object.assign<Options, Options>({
     logging: config.dev ? console.log : false // Логируем все SQL запросы в режиме разработки
-}));
+}, config.db));
 
 export * from './clean';
